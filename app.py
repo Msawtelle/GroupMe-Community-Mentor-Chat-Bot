11511ba6ed877b2501efa085c4fd151a94551c78
Mitchell <mitchell.sawtelle@okstate.edu>
2018-10-09 06:31:53 -0500

    maintenance hours

--- app.py
+++ app.py
@@ -176,14 +176,10 @@ def get_service(service_name='sheets', api_version='v4'):
 def check_maintenance_hours(current_datetime):
         #regular maintenace hours 8-5pm
         current_time = current_datetime.time()
-        print(current_datetime.weekday() in (5,6))
-        print(current_time >= t1 and current_time <=t2)
-        
         t1 = datetime.time(8,0)
         t2 = datetime.time(17,30)
-        if current_datetime.weekday() in (5,6):
-            return False
         if current_time >= t1 and current_time <= t2:
-            return True
+            if current_datetime.weekday() in (0,1,2,3,4):
+                return True
         else:
             return False
